060d06860b293a1106ee5f0864c0e179c5e3d4d3
Salvatore Guarino <sg@deved.it>
2015-03-10 16:29:25 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# magento-keen-io
-Magento module to send orders data to keen.io
+# Magento Keen IO
+Magento module to send orders data to [keen.io](http://keen.io/). The plugin supports the Magento Community and Enterprise edition.
+
+## Installation ##
+
+Magento Keen IO uses [Composer](http://getcomposer.org) and [Magento Composer Installer](https://github.com/magento-hackathon/magento-composer-installer) to handle installation of the module and its dependencies. To install Magento Keen IO you will need a copy of _composer.phar_ in your path. If you do not have it availble, run the following commands from your terminal.
+
+    $ curl -sS https://getcomposer.org/installer | php
+    $ chmod a+x composer.phar
+
+If you are already using Magento Composer Installer and have an existing _composer.json_, add _https://github.com/deved-it/magento-keen-io_ to the repositories list and _deved-it/magento-keen-io_ as a required dependency for your project. That's it!
+
+If you do not have an existing Magento Composer Installer _composer.json_ file defined, you can use the following template.
+
+    {
+      "repositories": [
+          {
+            "type":"composer",
+            "url":"http://packages.firegento.com"
+          },
+          {
+              "type": "vcs",
+              "url": "https://github.com/deved-it/magento-keen-io"
+          }
+      ],
+      "require": {
+          "magento-hackathon/magento-composer-installer": "*",
+          "deved/magento-keen-io"
+      },
+      "extra":{
+          "magento-root-dir":"./",
+          "magento-force":"true"
+      }
+    }
+
+To install Magento Keen IO and its dependencies just run composer.phar.
+
+    $ ./composer.phar install
+
+## Configuration ##
+
+Open the Magento Admin interface and go to configuration -> DEVED KEEN IO -> Keen IO Configuration and fill in the fields with your Project ID and Write key. When you will receive a new order, all order data will be sent to your keen.io project.
---
+++ app/code/community/Deved/KeenIo/Model/Observer.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ * Deved_KeenIo Module
+ *
+ * Copyright (c) 2015 Deved Sas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+use KeenIO\Client\KeenIOClient;
+class Deved_KeenIo_Model_Observer {
+
+    protected $projectId;
+    protected $writeKey;
+
+    public function __construct()
+    {
+        $this->projectId = Mage::getStoreConfig("keenio_section/keenio_group/project_id");
+        $this->writeKey = Mage::getStoreConfig("keenio_section/keenio_group/write_key");
+    }
+
+    public function sendNewOrder(Varien_Event_Observer $observer){
+      $this->loadLibraries();
+      $params = Mage::app()->getFrontController()->getRequest()->getParams();
+      $order = $observer->getEvent()->getOrder();
+      Mage::log('Order intercepted');
+      //use Keen IO Client to send order data
+      if ($this->projectId && $this->writeKey)
+      {
+          try {
+              $client = KeenIOClient::factory([
+                  'projectId' => $this->projectId,
+                  'writeKey'  => $this->writeKey
+              ]);
+              $client->addEvent('orders', $order->toArray());
+          } catch (Exception $e) {
+              Mage::logException($e);
+          }
+
+      }
+    }
+
+    /**
+     * Attempt to load geoPHP from lib and vendor directories
+     */
+    protected function loadLibraries()
+    {
+        $files = array(
+            Mage::getBaseDir().'/vendor/autoload.php'
+        );
+        foreach ($files as $file) {
+            if (file_exists($file)) {
+                require_once $file;
+                return;
+            }
+        }
+    }
+
+}
